726ed57d9df14859b144801168d9148e72e8ce96
fred-monier <frederico.monier@gmail.com>
2018-08-31 14:27:54 -0300

    Todas as chamadas ao controlador foram autenticadas. Toda a lógica de ambiente de produção, id e chave para acesso foi incorporada a essa aplicação.

---
+++ src/br/pe/recife/surfix/ecommerce/entity/Adquirente_.java
@@ -0,0 +1,13 @@
+package br.pe.recife.surfix.ecommerce.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2018-08-27T15:06:17.335-0300")
+@StaticMetamodel(Adquirente.class)
+public class Adquirente_ {
+	public static volatile SingularAttribute<Adquirente, Integer> id;
+	public static volatile SingularAttribute<Adquirente, String> nome;
+	public static volatile SingularAttribute<Adquirente, String> descricao;
+}
---
+++ src/br/pe/recife/surfix/ecommerce/entity/EmpresaAdquirente_.java
@@ -0,0 +1,13 @@
+package br.pe.recife.surfix.ecommerce.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2018-08-27T15:06:21.128-0300")
+@StaticMetamodel(EmpresaAdquirente.class)
+public class EmpresaAdquirente_ {
+	public static volatile SingularAttribute<EmpresaAdquirente, Integer> id;
+	public static volatile SingularAttribute<EmpresaAdquirente, String> mecId;
+	public static volatile SingularAttribute<EmpresaAdquirente, String> mecKey;
+}
